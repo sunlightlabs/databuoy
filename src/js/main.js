b600021c284d7a5cb3e390ad82b4aa6d6f2bc740
Sam Handler <sam.handler@gmail.com>
2015-07-24 12:06:02 -0400

    Proof React is rigged up

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 spreadsheet_data = Config.init(Data.downloadSpreadsheet);
+var Element = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Element.
+      </div>
+    );
+  }
+});
+React.render(
+  <Element />,
+  document.getElementById('content')
+);
