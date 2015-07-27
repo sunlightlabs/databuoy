c5d11ae4716f3db7d781e373f95231bec11faca6
Sam Handler <sam.handler@gmail.com>
2015-07-27 15:52:52 -0400

    Very light styling

--- src/js/components/datasets.js
+++ src/js/components/datasets.js
@@ -30,7 +30,34 @@ var DataList = React.createClass({
     });
     return (
       <table className="data_list">
-        <tbody>
+        <thead>
+          <tr>
+            <td className="title"><span className="label">title</span></td>
+            <td className="description"><span className="label">description</span></td>
+            <td className="keyword"><span className="label">keyword</span></td>
+            <td className="modified"><span className="label">modified</span></td>
+            <td className="publisher"><span className="label">publisher</span></td>
+            <td className="identifier"><span className="label">identifier</span></td>
+            <td className="accessLevel"><span className="label">accessLevel</span></td>
+            <td className="license"><span className="label">license</span></td>
+            <td className="rights"><span className="label">rights</span></td>
+            <td className="temporal"><span className="label">temporal</span></td>
+            <td className="spatial"><span className="label">spatial</span></td>
+            <td className="accrualPeriodicity"><span className="label">accrualPeriodicity</span></td>
+            <td className="conformsTo"><span className="label">conformsTo</span></td>
+            <td className="describedBy"><span className="label">describedBy</span></td>
+            <td className="describedByType"><span className="label">describedByType</span></td>
+            <td className="isPartOf"><span className="label">isPartOf</span></td>
+            <td className="issued"><span className="label">issued</span></td>
+            <td className="language"><span className="label">language</span></td>
+            <td className="landingPage"><span className="label">landingPage</span></td>
+            <td className="references"><span className="label">references</span></td>
+            <td className="theme"><span className="label">theme</span></td>
+            <td className="contactPoint_fn">contactPoint: full name</td>
+            <td className="contactPoint_hasEmail">contactPoint: hasEmail</td>
+          </tr>
+        </thead>
+        <tbody className="pure-table">
           {datasets}
         </tbody>
       </table>
@@ -42,27 +69,29 @@ var Dataset = React.createClass({
   render: function() {
     return (
       <tr>
-        <td className="title"><span className="label">title: </span>{this.props.data.title}</td>
-        <td className="description"><span className="label">description: </span>{this.props.data.description}</td>
-        <td className="keyword"><span className="label">keyword: </span>{this.props.data.keyword}</td>
-        <td className="modified"><span className="label">modified: </span>{this.props.data.modified}</td>
-        <td className="publisher"><span className="label">publisher: </span>{this.props.data.publisher}</td>
-        <td className="identifier"><span className="label">identifier: </span>{this.props.data.identifier}</td>
-        <td className="accessLevel"><span className="label">accessLevel: </span>{this.props.data.accessLevel}</td>
-        <td className="license"><span className="label">license: </span>{this.props.data.license}</td>
-        <td className="rights"><span className="label">rights: </span>{this.props.data.rights}</td>
-        <td className="temporal"><span className="label">temporal: </span>{this.props.data.temporal}</td>
-        <td className="spatial"><span className="label">spatial: </span>{this.props.data.spatial}</td>
-        <td className="accrualPeriodicity"><span className="label">accrualPeriodicity: </span>{this.props.data.accrualPeriodicity}</td>
-        <td className="conformsTo"><span className="label">conformsTo: </span>{this.props.data.conformsTo}</td>
-        <td className="describedBy"><span className="label">describedBy: </span>{this.props.data.describedBy}</td>
-        <td className="describedByType"><span className="label">describedByType: </span>{this.props.data.describedByType}</td>
-        <td className="isPartOf"><span className="label">isPartOf: </span>{this.props.data.isPartOf}</td>
-        <td className="issued"><span className="label">issued: </span>{this.props.data.issued}</td>
-        <td className="language"><span className="label">language: </span>{this.props.data.language}</td>
-        <td className="landingPage"><span className="label">landingPage: </span>{this.props.data.landingPage}</td>
-        <td className="references"><span className="label">references: </span>{this.props.data.references}</td>
-        <td className="theme"><span className="label">theme: </span>{this.props.data.theme}</td>
+        <td className="title">{this.props.data.title}</td>
+        <td className="description">{this.props.data.description}</td>
+        <td className="keyword">{this.props.data.keyword}</td>
+        <td className="modified">{this.props.data.modified}</td>
+        <td className="publisher">{this.props.data.publisher}</td>
+        <td className="identifier">{this.props.data.identifier}</td>
+        <td className="accessLevel">{this.props.data.accessLevel}</td>
+        <td className="license">{this.props.data.license}</td>
+        <td className="rights">{this.props.data.rights}</td>
+        <td className="temporal">{this.props.data.temporal}</td>
+        <td className="spatial">{this.props.data.spatial}</td>
+        <td className="accrualPeriodicity">{this.props.data.accrualPeriodicity}</td>
+        <td className="conformsTo">{this.props.data.conformsTo}</td>
+        <td className="describedBy">{this.props.data.describedBy}</td>
+        <td className="describedByType">{this.props.data.describedByType}</td>
+        <td className="isPartOf">{this.props.data.isPartOf}</td>
+        <td className="issued">{this.props.data.issued}</td>
+        <td className="language">{this.props.data.language}</td>
+        <td className="landingPage">{this.props.data.landingPage}</td>
+        <td className="references">{this.props.data.references}</td>
+        <td className="theme">{this.props.data.theme}</td>
+        <td className="contactPoint_fn">{this.props.data.contactPoint}</td>
+        <td className="contactPoint_hasEmail">{this.props.data.contactPoint}</td>
       </tr>
     );
   }
