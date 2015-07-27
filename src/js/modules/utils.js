53fff122e603292f55c9df47d22cd3ec10e4f682
Sam Handler <sam.handler@gmail.com>
2015-07-27 17:30:22 -0400

    setPageTitle and parseDescription methods

--- src/js/modules/utils.js
+++ src/js/modules/utils.js
@@ -4,5 +4,12 @@ var Utils = {
   },
   parseEmail: function(email_string) {
     return email_string.replace("mailto:", "");
+  },
+  parseDescription: function(description) {
+    return description.slice(0,137) + '...';
+  },
+  setPageTitle: function(page_title) {
+    $('title').text(page_title);
+    $('h1').text(page_title);
   }
 };
