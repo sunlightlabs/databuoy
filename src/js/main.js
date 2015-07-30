cfd6136ef82e91f6d738f87602586fa37019bfb9
Sam Handler <sam.handler@gmail.com>
2015-07-30 17:46:31 -0400

    Use React again

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,4 @@
 Config.init();
-//React.render(
-//  <App />, document.getElementById('content')
-//)
-    $.ajax({
-      context: this,
-      url: "sample-data.csv"
-    }).done(function(data) {
-      $("#content").text(data);
-      $('#loading').css({'display':'none'});
-      $('#content').css({'display':'block'});
-      Data.convertCSVtoJSON(data);
-    });
+React.render(
+  <App />, document.getElementById('content')
+)
