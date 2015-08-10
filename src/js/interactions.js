a8e013c482796cffaef287d765ce5d60f786f3c5
Sam Handler <sam.handler@gmail.com>
2015-08-10 14:54:23 -0400

    Export click binding

--- src/js/interactions.js
+++ src/js/interactions.js
@@ -1,3 +1,10 @@
 $('#header').click(function() {
   window.location = window.location.pathname;
 });
+$('#export').click(function() {
+  Data.downloadSpreadsheet().then(function(data) {
+    new_elem = $('<code>' + Data.exportDatasetAsJSON(data) + '</code>');
+    $('body').html('');
+    $('body').append(new_elem);
+  });
+});
